932cf63472c94354f63e63e0b046b955697a43fc
relaxdd <awenn2015@gmail.com>
2025-08-06 16:23:54 +0300

    Доработал главную страницу

---
+++ _public/script.js
@@ -0,0 +1,50 @@
+(function () {
+  const filter = document.querySelector('.filters');
+  const typeFilter = document.querySelector('#type-filter');
+  const yearFilter = document.querySelector('#date-filter');
+  const projects = document.querySelectorAll('#wrapper > li');
+
+  function replaceState(type, year) {
+    if (!type && !year) {
+      for (const project of projects) {
+        project.style.removeProperty('display');
+      }
+    } else {
+      for (const project of projects) {
+        const years = project.dataset?.['year'] ?? '';
+        const types = project.dataset?.['types'] ?? '';
+
+        if (type && !types) continue;
+        console.log({ type, types });
+
+        if (year && !years) continue;
+        console.log({ year, years });
+
+        const parse = types.split(';');
+        const display = (type ? parse.includes(type) : true) && years === year;
+
+        if (display) project.style.removeProperty('display');
+        else project.style.setProperty('display', 'none');
+      }
+    }
+  }
+
+  window.addEventListener('load', () => {
+    replaceState(typeFilter.value, yearFilter.value);
+  });
+
+  typeFilter.addEventListener('change', (e) => {
+    replaceState(e.target.value, yearFilter.value);
+  });
+
+  yearFilter.addEventListener('change', (e) => {
+    replaceState(typeFilter.value, e.target.value);
+  });
+
+  filter.addEventListener('reset', () => {
+    typeFilter.value = '';
+    yearFilter.value = '';
+
+    replaceState('', '');
+  });
+})();
